f93868e800f1f984a39af156a7220308c25caafa
austin-tabulog <austin.tabulog@metamorphsoftware.com>
2018-01-11 14:06:37 -0600

    edited two scripts and developed further XML openrocket building

--- clafer-models/Test/instance2xml.py
+++ clafer-models/Test/instance2xml.py
@@ -14,38 +14,69 @@ Output: XML formatted .ork files for every instance generated
 from sys import argv
 from os import path, mkdir, getcwd, listdir
 
-def findInstances(instance_dir):
-    # build an xml only for instance .txt files in the instance folder
-    for file in listdir(instance_dir):
-        if path.splitext(file)[1] == '.txt':
-            buildXML(file)
+class RocketXML:
+    def __init__(self, readfile, writefile):
+        self.readfile = readfile
+        self.writefile = writefile
 
-def buildXML(instance_file):
+
+    def build_start_tag(self):
+        self.writefile.write("<?xml version='1.0' encoding='utf-8'?>\n")
+        self.writefile.write('<openrocket version="1.0" creator="OpenRocket 15.03">\n')
+        self.writefile.write('\t<rocket> \n')
+        self.writefile.write('\t\t<name>Rocket</name> \n')
+        self.writefile.write('\t\t<referencetype>maximum</referencetype> \n')
+        self.writefile.write('\t\t<subcomponents> \n')
+        self.writefile.write('\t\t\t<stage>\n')
+        self.writefile.write('\t\t\t\t<name>Sustainer</name>\n')
+        self.writefile.write('\t\t\t\t<subcomponents>\n')
+
+    def nosecone(self):
+        pass
+
+    def build_body(self, line):
+        self.line = line
+        catch_body = '  body\n'
+        if line == catch_body:
+            self.writefile.write('\t\t\t\t  <bodytube>\n')
+            self.writefile.write('\t\t\t\t    <name>Body tube</name>\n')
+            self.writefile.write('\t\t\t\t    <finish>normal</finish>\n')
+            self.writefile.write('\t\t\t\t    <material type="bulk" density="6100.0">Carbon fiber</material>\n')
+            self.writefile.write('\t\t\t\t    <length>0.2</length>\n')
+            self.writefile.write('\t\t\t\t    <thickness>0.002</thickness>\n')
+            self.writefile.write('\t\t\t\t    <radius>auto</radius>\n')
+
+def listXML(instance_dir):
     # Create new XML directory if necessary
-    XML_dir = getcwd()+'\XML'
+    XML_dir = instance_dir.replace('Instances','XML')
     if not path.exists(XML_dir):
         mkdir(XML_dir)
 
-    # create file
-    xmlfile = open(path.join(XML_dir, "{}{}".format(path.splitext(instance_file)[0],".ork")),'w')
-    xmlfile.close()
-
-    #create start tags
-    xmlfile.write(RocketXML.start_tag(instance_file, xmlfile))
-
-    #reference clafer components with subcomponents to a rocket builder class
-
-
-findInstances(getcwd()+'\Instances')
+    # build an xml only for instance .txt files in the instance folder
+    for file in listdir(instance_dir):
+        if path.splitext(file)[1] == '.txt':
+            # create file
+            instance_name = path.splitext(file)[0]
+            xml_file = open(path.join(XML_dir, "{}{}".format(instance_name,".ork")),'w')
+            #xml_file.close()
+            populateXML(path.join(instance_dir,file), xml_file)
 
 
+def populateXML(readfile, writefile):
+    #create RocketXML instance
+    instance = RocketXML(readfile, writefile)
+    #write start tag
+    RocketXML.build_start_tag(instance)
+    with open(readfile) as read:
+        #import pdb; pdb.set_trace()
+        catch_body = "  body\n"
+        for line in read:
+            #import pdb; pdb.set_trace()
+            if line == catch_body:
+                RocketXML.build_body(instance, line)
+            else:
+                pass
 
+#reference clafer components with subcomponents to a rocket builder class
 
-
-
-
-
-class RocketXML:
-    def __init__(self):
-
-    def start_tag(readfile, writefile)
+listXML(getcwd()+'\Instances')
