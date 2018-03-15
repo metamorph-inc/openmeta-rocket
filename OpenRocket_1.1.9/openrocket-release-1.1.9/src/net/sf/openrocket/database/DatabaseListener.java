eeb3b687bda391316bc0f0eedf5b1bf017466f62
Cailey Cline <cicline@mail.lipscomb.edu>
2018-03-15 11:45:48 -0500

    Cleaned up OpenRocket_1.1.9 and started demo PET script

--- OpenRocket_1.1.9/openrocket-release-1.1.9/src/net/sf/openrocket/database/DatabaseListener.java
+++
@@ -1,9 +0,0 @@
-package net.sf.openrocket.database;
-
-public interface DatabaseListener<T extends Comparable<T>> {
-
-	public void elementAdded(T element, Database<T> source);
-	
-	public void elementRemoved(T element, Database<T> source);
-	
-}
--- OpenRocket_1.1.9/openrocket-release-1.1.9/src/net/sf/openrocket/file/openrocket/savers/ComponentAssemblySaver.java
+++
@@ -1,7 +0,0 @@
-package net.sf.openrocket.file.openrocket.savers;
-
-public class ComponentAssemblySaver extends RocketComponentSaver {
-
-	// No-op
-	
-}
